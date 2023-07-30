5ceca15d40581b7061075a31425fdd88db3211dd
Kaptan <sukhvinderkaulapur@gmmail.com>
2023-07-30 17:06:21 +0530

    create user

--- home/serializers.py
+++ home/serializers.py
@@ -8,12 +8,17 @@ class RegisterSerializer(serializers.Serializer):
     password = serializers.CharField()
     def validate(self,data):
         if data['username']:
-            if User.objects.filter(username=data['username']).exist():
+            if User.objects.filter(username=data['username']).exists():
                 raise serializers.ValidationError('username is taken')
         if data['email']:
-            if User.objects.filter(email=data['email']).exist():
+            if User.objects.filter(email=data['email']).exists():
                 raise serializers.ValidationError('email is taken')    
         return data
+    
+    def create(self, validated_data):
+        user = User.objects.create(username=validated_data['username'],email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        return validated_data
 
 class LoginSerializer(serializers.Serializer):
     email= serializers.EmailField()
